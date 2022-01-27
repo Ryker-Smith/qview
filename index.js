47303b2813aa6f1b5b4c832b58d15c4e4474a0bf
root <root@server1115.stsheelans.local>
2022-01-27 14:25:52 +0000

    First

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5050
+
+app.get('/', (req, res) => {
+  res.send('\'sMuire duit, dude,')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
